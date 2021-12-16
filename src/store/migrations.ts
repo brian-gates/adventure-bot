dcf6f3adc9311449a74c006f07322a2c7c7a9fde
Brian Gates <brian.g.gates@gmail.com>
2021-12-16 10:21:01 -0800

    wip migration

--- src/store/migrations.ts
+++ src/store/migrations.ts
@@ -1,16 +1,36 @@
 import { RootReducerState } from ".";
 import { createMigrate } from "redux-persist";
 import { defaultEncounterWeights } from "./slices/encounters";
+import { mapValues } from "remeda";
+import { Item } from "../equipment/Item";
 
 /*
  * This is the current version and should match the latest version
  */
-export const persistVersion = 2;
+export const persistVersion = 4;
 
 /**
  * Here we use RootReducerState instead of ReduxState to avoid cyclical type references
  */
-type PersistedReduxStateV3 = RootReducerState;
+type PersistedReduxStateV4 = RootReducerState;
+
+// normalization of inventories
+type PersistedReduxStateV3 = Omit<PersistedReduxStateV4, "characters"> & {
+  characters: Omit<
+    PersistedReduxStateV4["characters"],
+    "itemsById" | "charactersById"
+  > & {
+    charactersById: Record<
+      string,
+      Omit<
+        PersistedReduxStateV4["characters"]["charactersById"][string],
+        "inventory"
+      > & {
+        inventory: Item[];
+      }
+    >;
+  };
+};
 
 // state prior to stateful encounter weights
 type PersistedReduxStateV2 = Omit<PersistedReduxStateV3, "encounterWeights">;
@@ -21,8 +41,6 @@ type PersistedReduxStateV1 = Omit<PersistedReduxStateV2, "characters"> & {
 };
 
 type MigrationState = PersistedReduxStateV1 | PersistedReduxStateV2;
-
-/** Migrations **/
 
 const persistMigrations = {
   2: (state: PersistedReduxStateV1): PersistedReduxStateV2 => ({
@@ -37,6 +55,18 @@ const persistMigrations = {
     encounters: {
       ...state.encounters,
       encounterWeights: defaultEncounterWeights,
+    },
+  }),
+  // normalize inventories
+  4: (state: PersistedReduxStateV3): PersistedReduxStateV4 => ({
+    ...state,
+    characters: {
+      ...state.characters,
+      ...mapValues(state.characters.charactersById, (character) => ({
+        ...character,
+        inventory: character.inventory.map((item) => item.id),
+      })),
+      itemsById: {},
     },
   }),
 };
